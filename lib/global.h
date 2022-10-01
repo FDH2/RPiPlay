c8c882680f4983328598959c229a4c858e5ce684
fduncanh <fduncanh@gmail.com>
2022-10-01 15:00:52 -0400

    Add LGPL headers to files from Shairplay which are missing them Modifications (c) (2019-21) Florian Draschbacher and contributors

--- lib/global.h
+++ lib/global.h
@@ -1,3 +1,23 @@
+/**
+ *  This code originates from the Shairplay project
+ *  https://github.con/juvoh/shairplay by Juho Vähä-Herttua and
+ *  contributors (2011-2019), and is used under permission of that project's
+ *  GNU Lesser General Public License (as described below) in the project:
+ *
+ *  RPiPlay - An open-source AirPlay mirroring server for Raspberry Pi
+ *  Modifications (C) Florian Draschbacher and contributors (2019-2021)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ */
+
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
